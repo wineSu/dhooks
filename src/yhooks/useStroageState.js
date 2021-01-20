8c1318ce13256b3d397d9f03af259f9979b53c32
wineSu <suzhe@yonyou.com>
2021-01-20 11:50:21 +0800

    change

--- src/yhooks/useStroageState.js
+++ src/yhooks/useStroageState.js
@@ -3,7 +3,7 @@ import { useCallback, useState } from 'react';
 export function useCookieState(cookieKey, {dafaultVal, expire}) {
     const [state, setState] = useState(() => {
         return getCookie(cookieKey) || dafaultVal;
-    });
+    }); 
 
     const updateState = useCallback(
         (newValue) => {
