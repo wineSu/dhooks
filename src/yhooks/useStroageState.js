693ca9a96da7bc59517cb8e93e0edf67db9dd9e8
wineSu <suzhe@yonyou.com>
2021-01-20 11:37:30 +0800

    localstorage

---
+++ src/yhooks/useStroageState.js
@@ -0,0 +1,19 @@
+import { useCallback, useState } from 'react';
+
+export function useCookieState(cookieKey, {dafaultVal, expire}) {
+    const [state, setState] = useState(() => {
+        return getCookie(cookieKey) || dafaultVal;
+    });
+
+    const updateState = useCallback(
+        (newValue) => {
+            setState(() => {
+                setCookie(cookieKey, newValue, newValue ? expire : -1);
+                return newValue;
+            });
+        },
+        [cookieKey, expire]
+    );
+
+    return [state, updateState];
+}
