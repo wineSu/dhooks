38d37529f31780000202c95019a7c31a64bc7004
wineSu <1451894521@qq.com>
2021-05-13 11:24:32 +0800

    增加图表依赖项处理

--- src/dhooks/useCharts.jsx
+++ src/dhooks/useCharts.jsx
@@ -1,25 +1,27 @@
-import {
-  useRef,
-  useEffect,
-} from 'react';
+import { useRef, useEffect } from "react";
 import * as echarts from "echarts";
+import { useDeepCompareEffect } from './compare';
 
 /**
-* echarts使用
-* @param {*} id 
-* @param {*} option 
-*/
-export function useCharts(id, option = {}) {
-
+ * echarts使用
+ * @param {*} id
+ * @param {*} option
+ * @param {*} dep
+ */
+export function useCharts(id, option = {}, dep) {
   const { current } = useRef({});
 
+  useDeepCompareEffect(() => {
+    current.render && current.render.setOption(option)
+  }, dep)
+
   useEffect(() => {
-      let mapInit = echarts.init(document.getElementById(id));
-      mapInit.setOption(option);
-      current.render = mapInit;
-      return () => {
-        mapInit.dispose();
-      }
+    let mapInit = echarts.init(document.getElementById(id));
+    mapInit.setOption(option);
+    current.render = mapInit;
+    return () => {
+      mapInit.dispose();
+    };
   }, []);
 
   return current;
